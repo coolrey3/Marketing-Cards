8689b804f716cccd5d1e917bfafffa3a1e10070d
Ray Mercedes <contact@RayMercedes.com>
2018-10-24 12:50:06 -0400

    change src for images

--- marketingcardresult.php
+++ marketingcardresult.php
@@ -9,7 +9,7 @@ $type=$_POST["type"];
 echo "<div style='position:relative;'>";
     ######## Background picture ############
     echo "<div style=';text-align:center;margin:auto;position:absolute;float:left;'>";
-        echo " <img style='z-index:-8;width:4in;height:6in;' src='http://mobilerepairtechs.com/mcbackground1.png'";
+        echo " <img style='z-index:-8;width:4in;height:6in;' src='./images/mcbackground1.png'";
     echo "</div>";
 
      ############ Product Price ###########
@@ -27,7 +27,7 @@ echo "<div style='position:relative;'>";
     ############ Type##########
 if($type ==='Game') {
     echo "<div style='z-index:10;position:absolute;top:-50px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='http://mobilerepairtechs.com/games.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='./images/games.png' ></h1>". "";
     echo "</div>";
     };
 
@@ -43,56 +43,56 @@ if($carrier ==='T-Mobile') {
 
 if($carrier ==='AT&T') {
     echo "<div style='z-index:10;position:absolute;top:350px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:2in;' src='http://mobilerepairtechs.com/attt.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:2in;' src='./images/attt.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='Cricket') {
     echo "<div style='z-index:10;position:absolute;top:350px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='http://mobilerepairtechs.com/cricket.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='./images/cricket.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='Unlocked') {
     echo "<div style='z-index:10;position:absolute;top:335px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:1.2in;' src='http://mobilerepairtechs.com/Unlocked2.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:1.2in;' src='./images/Unlocked2.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='Verizon') {
     echo "<div style='z-index:10;position:absolute;top:320px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:2.0in;' src='http://mobilerepairtechs.com/verizon.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:2.0in;' src='./images/verizon.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='MetroPCS') {
     echo "<div style='z-index:10;position:absolute;top:360px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='http://mobilerepairtechs.com/metro.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='./images/metro.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='Boost') {
     echo "<div style='z-index:10;position:absolute;top:360px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='http://mobilerepairtechs.com/boost.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:3in;' src='./images/boost.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='StraightTalk') {
     echo "<div style='z-index:10;position:absolute;top:345px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:1.7in;' src='http://mobilerepairtechs.com/straighttalk.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:1.7in;' src='./images/straighttalk.png' ></h1>". "";
     echo "</div>";
     };
 
 if($carrier ==='Wifi') {
     echo "<div style='z-index:10;position:absolute;top:340px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:1.2in;' src='http://mobilerepairtechs.com/wifi1.png' ></h1>". "";
+        echo "<h1 style='top:55px;position:relative;z-index:6;height:;'><img style='width:1.2in;' src='./images/wifi1.png' ></h1>". "";
     echo "</div>";
     };
 
 
 if($carrier ==='Sprint') {
     echo "<div style='z-index:10;position:absolute;top:360px;float:left;width:100%;color:white;text-align:center'>";
-        echo "<h1 style='top:25px;position:relative;z-index:6;height:;'><img style='width:3in;' src='http://mobilerepairtechs.com/sprint2.png' ></h1>". "";
+        echo "<h1 style='top:25px;position:relative;z-index:6;height:;'><img style='width:3in;' src='./images/sprint2.png' ></h1>". "";
     echo "</div>";
     };
 
